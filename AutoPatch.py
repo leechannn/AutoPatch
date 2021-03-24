e741a59ae9d547a1ce3983e7a3cc5842e04707a4
leechannn <lchk0414@gmail.com>
2021-03-24 22:24:43 +0900

    Language List

--- AutoPatch.py
+++ AutoPatch.py
@@ -2,27 +2,61 @@ import sys
 
 
 def distinguish_lang(source: str) -> str:
-    frame = "PHP"
-    return frame
+    #Assume that the language used has already been entered.
+    if int(sys.argv[1]) == 0:
+        used_lang = "PHP"
+    elif int(sys.argv[1]) == 1:
+        used_lang = "ASP.NET"
+    elif int(sys.argv[1]) == 2:
+        used_lang = "Ruby"
+    elif int(sys.argv[1]) == 3:
+        used_lang = "Java"
+    elif int(sys.argv[1]) == 4:
+        used_lang = "Scala"
+    elif int(sys.argv[1]) == 5:
+        used_lang = "Python"
+    elif int(sys.argv[1]) == 6:
+        used_lang = "JavaScript"
+    elif int(sys.argv[1]) == 7:
+        used_lang = "perl"
+    else:
+        print("Error: Undefined language type")
+        sys.exit()
+    return used_lang
 
 
 def get_vuln_type() -> str:
-    vuln_name = "SQL_Injection"
+    if int(sys.argv[2]) == 0:
+        vuln_name = "SQL_Injection"
+    elif int(sys.argv[2]) == 1:
+        vuln_name = "XSS"
+    elif int(sys.argv[2]) == 2:
+        vuln_name = "File Inclusion"
+    elif int(sys.argv[2]) == 3:
+        vuln_name = "Command Injection"
+    else:
+        print("Error: Undefined vulnerability type")
+        sys.exit()
     return vuln_name
 
 
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: python AutoPatch.py [index]")
-        sys.exit()
+def find_insert_index(code: str, frame: str) -> int:
+    index = 0
 
-    location_index = int(sys.argv[1])
+    return index
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print("Usage: python AutoPatch.py [Language] [vulnerability type]")
+        sys.exit()
 
     with open("code.txt", "r") as f:
         data = f.read()
 
     lang = distinguish_lang(data)
     vuln_type = get_vuln_type()
+    location_index = find_insert_index(data, lang)
 
     with open("./vulnList/" + vuln_type + "-" + lang + ".txt", "r") as f:
         patch_code = f.read()
