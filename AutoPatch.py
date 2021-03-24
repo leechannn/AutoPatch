1756588f744e75a1c6ec4ce0f61d8a8a486344a7
leechannn <lchk0414@gmail.com>
2021-03-24 20:35:43 +0900

    Basic structure

--- AutoPatch.py
+++ AutoPatch.py
@@ -1,13 +1,23 @@
+import sys
+
+
 def distinguish_lang(source: str) -> str:
     frame = "PHP"
     return frame
 
-def get_vuln_type():
-    vuln_type = "SQL_Injection"
-    return vuln_type
+
+def get_vuln_type() -> str:
+    vuln_name = "SQL_Injection"
+    return vuln_name
 
 
 if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: python AutoPatch.py [index]")
+        sys.exit()
+
+    location_index = int(sys.argv[1])
+
     with open("code.txt", "r") as f:
         data = f.read()
 
@@ -17,6 +27,5 @@ if __name__ == '__main__':
     with open("./vulnList/" + vuln_type + "-" + lang + ".txt", "r") as f:
         patch_code = f.read()
 
-
-
-
+    result = data[:location_index] + patch_code + data[location_index:]
+    print(result)
