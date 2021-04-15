4291ce534f7073e7f17c398112f0e771e00d3429
leechannn <lchk0414@gmail.com>
2021-04-15 20:27:26 +0900

    resume

--- AutoPatch.py
+++ AutoPatch.py
@@ -1,5 +1,16 @@
 import sys
 
+class Patch:
+    def __init__(self):
+        self.func = ''
+        self.reg = ''
+
+    def set_func(self, func):
+        self.func = func
+
+    def set_reg(self, reg):
+        self.reg = reg.strip()
+
 
 def distinguish_lang() -> str:
     #Assume that the language used has already been entered.
@@ -18,7 +29,7 @@ def distinguish_lang() -> str:
     elif int(sys.argv[1]) == 6:
         used_lang = "JavaScript"
     elif int(sys.argv[1]) == 7:
-        used_lang = "perl"
+        used_lang = "Perl"
     else:
         print("Error: Undefined language type")
         sys.exit()
@@ -27,7 +38,7 @@ def distinguish_lang() -> str:
 
 def get_vuln_type() -> str:
     if int(sys.argv[2]) == 0:
-        vuln_name = "SQL_Injection"
+        vuln_name = "SQL Injection"
     elif int(sys.argv[2]) == 1:
         vuln_name = "XSS"
     elif int(sys.argv[2]) == 2:
@@ -38,6 +49,33 @@ def get_vuln_type() -> str:
         print("Error: Undefined vulnerability type")
         sys.exit()
     return vuln_name
+
+
+def get_patch_info(lang, vuln_type):
+    with open('./vulnList/' + vuln_type + "-" + lang + ".txt", 'r') as f:
+        lines = f.readlines()
+
+    text_type = ''
+    vuln = Patch()
+    tmp_str = ''
+
+    for line in lines:
+        if line[:10] == '%%%%%%%%%%' and text_type == '':
+            text_type = line[10:14]
+        elif line[:10] == '%%%%%%%%%%' and text_type == 'FUNC':
+            vuln.set_func(tmp_str)
+            text_type = ''
+            tmp_str = ''
+        elif line[:10] == '%%%%%%%%%%' and text_type == 'DECR':
+            vuln.set_reg(tmp_str)
+            text_type = ''
+            tmp_str = ''
+        else:
+            tmp_str = tmp_str + line
+
+    print(vuln.func)
+    print(vuln.reg)
+    return vuln
 
 
 def find_insert_index(code: str, frame: str) -> int:
@@ -56,10 +94,11 @@ if __name__ == '__main__':
 
     lang = distinguish_lang()
     vuln_type = get_vuln_type()
-    location_index = find_insert_index(data, lang)
+    vuln_patch_info = get_patch_info(lang, vuln_type)
+    '''location_index = find_insert_index(data, lang)
 
     with open("./vulnList/" + vuln_type + "-" + lang + ".txt", "r") as f:
         patch_code = f.read()
 
     result = data[:location_index] + patch_code + data[location_index:]
-    print(result)
+    print(result)'''
